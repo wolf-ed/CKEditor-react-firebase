c7da13eb959fb32646ad357d4ea20fab2534e665
wolf-ed <wolf7ed@gmail.com>
2022-03-26 00:45:15 +0100

    responsive navbar and better file structure

--- src/App.js
+++ src/App.js
@@ -1,32 +1,52 @@
 import React, { useState } from 'react';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import Home from './pages/Home';
-import CreatePost from './pages/CreatePost';
-import Login from './pages/Login';
+import Home from './components/Home/Home';
+import CreatePost from './components/CreatePost/CreatePost';
+import Login from './components/Login/Login';
 import { signOut } from 'firebase/auth';
 import { auth } from './firebase-configuration';
 import { collection } from 'firebase/firestore';
-import { db } from "./firebase-configuration";
+import { db } from './firebase-configuration';
+import { HiMenu } from 'react-icons/hi'
 
 function App() {
   const [isAuth, setIsAuth] = useState(localStorage.getItem('isAuth'));
+  const [navbarOptionsClass, setNavbarOptionsClass] = useState('navbar-options')
 
   const signUserOut = () => {
     signOut(auth).then(() => {
       localStorage.clear();
       setIsAuth(false);
       window.location.pathname = "/login";
+      toggleNavbarClass()
     })
   };
 
-  return (<Router>
-    <nav>
-      <Link to="/">Home</Link>
+  const toggleNavbarClass = () => {
+    if (navbarOptionsClass === 'navbar-options') {
+      setNavbarOptionsClass('navbar-options active')
+    } else {
+      setNavbarOptionsClass('navbar-options')
+    }
+  }
 
-      {!isAuth
-        ? <Link to="/login">Login</Link>
-        : <><Link to="/createpost">Create Post</Link> <button className='logoutBtn' onClick={signUserOut}>Log Out</button></>}
+  return (<Router>
+    <nav className='navbar'>
+      <a className='title' href="https://github.com/wolf-ed" target="_blank" rel="noopener noreferrer">GitHub</a>
+      <button className="toggle-button"
+        onClick={toggleNavbarClass}
+      >
+        <HiMenu />
+      </button>
+      <div className={navbarOptionsClass}>
+        <Link to="/" onClick={toggleNavbarClass}>Home</Link>
+
+        {!isAuth
+          ? <Link to="/login" onClick={toggleNavbarClass}>Login</Link>
+          : <><Link to="/createpost" onClick={toggleNavbarClass}>Create Post</Link> <div className='logoutBtn' onClick={signUserOut}>Logout</div></>}
+
+      </div>
     </nav>
     <Routes>
       <Route path="/" element={<Home isAuth={isAuth} />} />
---
+++ src/components/Login/Login.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { auth, provider } from '../../firebase-configuration';
+import { signInWithPopup } from 'firebase/auth';
+import { useNavigate } from "react-router-dom";
+import './Login.css'
+
+const Login = ({ setIsAuth }) => {
+    let navigate = useNavigate()
+
+    const signInWithGoogle = () => {
+        signInWithPopup(auth, provider).then((result) => {
+            localStorage.setItem('isAuth', true)
+            setIsAuth(true)
+            navigate("/")
+        })
+    }
+
+    return <div className="loginPage">
+        <p>Sign in with Google to continue</p>
+        <button
+            className="login-with-google-btn"
+            onClick={signInWithGoogle}
+        >Sign in with Google</button>
+    </div>
+}
+
+export default Login;
